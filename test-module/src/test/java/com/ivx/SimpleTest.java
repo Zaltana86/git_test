ca0083e855fe1fdc3872d5dc8c0fcc56b50f9cc3
Skyler <kl142857h@163.com>
2023-05-10 16:30:36 +0800

    初始化

---
+++ test-module/src/test/java/com/ivx/SimpleTest.java
@@ -0,0 +1,18 @@
+package com.ivx;
+
+
+import org.junit.Test;
+
+/**
+ * @author skyler
+ * @apiNote
+ * @since 2023/5/10 16:29
+ */
+
+public class SimpleTest {
+    @Test
+    public void test1() {
+        System.out.println(1);
+    }
+
+}
